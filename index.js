31b9cb36533d35fc59ea1bf7a8cfcd88a6355de2
pemrouz <p3pedram@hotmail.com>
2018-05-04 01:21:20 +0100

    feat: always stop via iter rather input

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-module.exports = fn => (next, i, t) =>
-  fn && fn.then ? (fn.then(i.stop), next)
-: fn && fn.each ? (fn.each(i.stop), next)
+module.exports = fn => (next, t) =>
+  fn && fn.then ? (fn.then(t.stop), next)
+: fn && fn.each ? (fn.each(t.stop), next)
 : (acc, v) => 
     typeof fn == 'number'   ? ((--fn <= 0 && t.stop()), (fn >= 0 ? next(acc,v) : acc))
   : typeof fn == 'function' ? (fn(v) ? (t.stop(), acc) : next(acc,v)) 
